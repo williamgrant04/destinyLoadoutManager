1619772d352632cae8ced3fe9f1a982818469505
William Grant <williamg3575467@gmail.com>
2024-04-20 22:40:18 +0100

    Install typescript and change all project files to reflect change (.ts/.tsx extension)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { APIContextProvider } from './store/bungieAPIContext.jsx'
+import App from './App.js'
+import { APIContextProvider } from './store/bungieAPIContext.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
